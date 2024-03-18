40f7c0845c648dee1a8358d36e09cb4bc042ae73
Leandro <levsistemasit@gmail.com>
2024-03-18 11:44:17 -0300

    Actualizacion, lectura de mensajes y respuestas

--- god_devil.js
+++ god_devil.js
@@ -3,16 +3,25 @@ const Discord = require('discord.js');
 
 //agregar keep_alive
 const keep_alive = require ('./keep_alive.js');
+require ('./keep_alive.js');
 
 //definir cliente
 const Client = new Discord.Client({
-    intents: 33027
+    intents: 33539
 });
 
 // contenido
 Client.on('ready', async ( client ) => {
     console.log(`Estoy listo!!! Hola Mundo, yo soy: ${client.user.tag}`)
 });
+
+Client.on('messageCreate', message => {
+    if (message.content === '!Hola'){
+        console.log('saludemos...')
+        message.reply('¡Hola!, ¿Como te encuentras?');
+        console.log('¡Hola!, ¿Como te encuentras?')
+    }
+})
 
 // conectar
 Client.login(process.env.token_god_devil)
